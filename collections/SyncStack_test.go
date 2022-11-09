eb9a549d0db5b5e9f66c1a821682111904b5d24e
sean <seanbatten58@gmail.com>
2022-11-09 19:06:29 +0000

    Tests

---
+++ collections/SyncStack_test.go
@@ -0,0 +1,125 @@
+package collections
+
+import (
+	"testing"
+)
+
+func Test_SyncStack_Empty(t *testing.T) {
+	stack := NewSyncStack[int]()
+
+	if stack.Count() != 0 {
+		t.Error("stack should have zero count")
+	}
+
+	if !stack.IsEmpty() {
+		t.Error("stack should be empty")
+	}
+
+	if _, popped := stack.Pop(); popped {
+		t.Error("shouldn't be able to pop anything!")
+	}
+
+	if _, peeked := stack.Peek(); peeked {
+		t.Error("shouldn't be able to peek anything!")
+	}
+}
+
+func Test_SyncStack_WithData(t *testing.T) {
+	stack := NewSyncStack[int]()
+
+	stack.Push(10)
+
+	if stack.Count() != 1 {
+		t.Error("stack should have 1 item")
+	}
+
+	if stack.IsEmpty() {
+		t.Error("stack isn't empty empty")
+	}
+
+	if value, peeked := stack.Peek(); !peeked || value != 10 {
+		t.Error("should have peeked 10")
+	}
+
+	if value, popped := stack.Pop(); !popped || value != 10 {
+		t.Error("should have popped 10")
+	}
+
+	if stack.Count() != 0 {
+		t.Error("stack should be empty")
+	}
+}
+
+func Test_SyncStack_Multiple(t *testing.T) {
+	stack := NewSyncStack[int]()
+
+	stack.Push(10).Push(20).Push(30)
+
+	if stack.Count() != 3 {
+		t.Error("stack should have 3 items")
+	}
+
+	if stack.IsEmpty() {
+		t.Error("stack isn't empty empty")
+	}
+
+	if value, popped := stack.Pop(); !popped || value != 30 {
+		t.Error("should have popped 30")
+	}
+
+	if value, popped := stack.Pop(); !popped || value != 20 {
+		t.Error("should have popped 20")
+	}
+
+	if value, popped := stack.Pop(); !popped || value != 10 {
+		t.Error("should have popped 10")
+	}
+
+	if stack.Count() != 0 {
+		t.Error("stack should be empty")
+	}
+}
+
+func Test_SyncStack_Clear(t *testing.T) {
+	stack := NewSyncStack[int]()
+
+	stack.Push(10).Push(20).Push(30)
+
+	if stack.Count() != 3 {
+		t.Error("stack should have 3 items")
+	}
+
+	stack.Clear()
+
+	if stack.Count() != 0 {
+		t.Error("stack should be empty")
+	}
+
+	if _, popped := stack.Pop(); popped {
+		t.Error("shouldn't have popped anything")
+	}
+}
+
+func Test_SyncStack_Stream(t *testing.T) {
+	stack := NewSyncStack[int]()
+
+	stack.Push(10).Push(20).Push(30)
+	i := stack.Stream().Iterator()
+
+	for _, value := range []int{30, 20, 10} {
+		if !i.MoveNext() {
+			t.Error("should have moved")
+			return
+		}
+
+		if i.Current() != value {
+			t.Errorf("should have %d", value)
+			return
+		}
+	}
+
+	if i.MoveNext() {
+		t.Error("shouldn't be able to move")
+		return
+	}
+}

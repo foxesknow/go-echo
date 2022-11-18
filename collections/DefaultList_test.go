13080287e1b4afe0b4fb668f4a97d6aa03c03762
sean <seanbatten58@gmail.com>
2022-11-18 11:01:08 +0000

    Added RemoveAt

--- collections/DefaultList_test.go
+++ collections/DefaultList_test.go
@@ -197,3 +197,45 @@ func Test_List_Stream(t *testing.T) {
 		t.Error("there shouldn't be any more data")
 	}
 }
+
+func Test_List_RemoveAt(t *testing.T) {
+	list := NewList[int]()
+
+	list.Add(10)
+	list.Add(20)
+	list.Add(30)
+	list.Add(40)
+
+	if err := list.RemoveAt(1); err != nil {
+		t.Error("should have removed item")
+	}
+
+	if item, _ := list.Get(0); item != 10 {
+		t.Error("expected 10")
+	}
+
+	if item, _ := list.Get(1); item != 30 {
+		t.Error("expected 30")
+	}
+
+	if item, _ := list.Get(2); item != 40 {
+		t.Error("expected 40")
+	}
+
+	if list.Count() != 3 {
+		t.Error("should have 3 items")
+	}
+}
+
+func Test_List_RemoveAt_BadIndex(t *testing.T) {
+	list := NewList[int]()
+
+	list.Add(10)
+	list.Add(20)
+	list.Add(30)
+	list.Add(40)
+
+	if err := list.RemoveAt(4); err == nil {
+		t.Error("should have failed to removed item")
+	}
+}

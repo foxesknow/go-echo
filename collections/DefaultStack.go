f5762590ecf2f18ea030944400555e9f99e77629
sean <seanbatten58@gmail.com>
2023-08-24 11:39:22 +0100

    Use slice rather than nodes

--- collections/DefaultStack.go
+++ collections/DefaultStack.go
@@ -5,51 +5,35 @@ import (
 )
 
 type defaultStack[T any] struct {
-	head  *stackNode[T]
-	count int
-}
-
-type stackNode[T any] struct {
-	Value T
-	Next  *stackNode[T]
+	data []T
 }
 
 // Creates a new stack that is not thread safe
 func NewStack[T any]() Stack[T] {
 	return &defaultStack[T]{
-		head:  nil,
-		count: 0,
+		data: nil,
 	}
 }
 
 // Removes all items from the stack
 func (self *defaultStack[T]) Clear() {
-	self.head = nil
-	self.count = 0
+	self.data = nil
 }
 
 // Pushes a new item onto the top of the stack
 func (self *defaultStack[T]) Push(value T) Stack[T] {
-	node := &stackNode[T]{
-		Value: value,
-		Next:  self.head,
-	}
-
-	self.head = node
-	self.count++
-
+	self.data = append(self.data, value)
 	return self
 }
 
 // Attempts to remove on item from the top of the stack
 func (self *defaultStack[T]) Pop() (value T, popped bool) {
-	if self.count != 0 {
-		oldHead := self.head
+	length := len(self.data)
+	if length != 0 {
+		item := self.data[length-1]
+		self.data = self.data[0 : length-1]
 
-		self.head = oldHead.Next
-		self.count--
-
-		return oldHead.Value, true
+		return item, true
 	}
 
 	return value, false
@@ -57,8 +41,9 @@ func (self *defaultStack[T]) Pop() (value T, popped bool) {
 
 // Attempts to return the top of the stack without removing it
 func (self *defaultStack[T]) Peek() (value T, peeked bool) {
-	if self.count != 0 {
-		return self.head.Value, true
+	length := len(self.data)
+	if length != 0 {
+		return self.data[length-1], true
 	}
 
 	return value, false
@@ -66,29 +51,30 @@ func (self *defaultStack[T]) Peek() (value T, peeked bool) {
 
 // Returns the number of items in the ctack
 func (self *defaultStack[T]) Count() int {
-	return self.count
+	return len(self.data)
 }
 
 // Returns true if the stack is empty, otherwise false
 func (self *defaultStack[T]) IsEmpty() bool {
-	return self.count == 0
+	return len(self.data) == 0
 }
 
 func (self *defaultStack[T]) Stream() data.Stream[T] {
-	if self.count == 0 {
+	if len(self.data) == 0 {
 		return data.EmptyStream[T]()
 	}
 
 	return &data.FunctionStream[T]{
 		OnIterator: func() data.Iterator[T] {
-			head := self.head
+			slice := self.data
+			index := len(slice) - 1
 			var current T
 
 			return &data.FunctionIterator[T]{
 				OnMoveNext: func() bool {
-					if head != nil {
-						current = head.Value
-						head = head.Next
+					if index != -1 {
+						current = self.data[index]
+						index--
 
 						return true
 					}
